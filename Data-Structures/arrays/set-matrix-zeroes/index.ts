9d8a4f8d200225314cfce9afda4d8536ed27706b
Faheem Khan <faheemkhan4865@gmail.com>
2024-08-17 22:52:29 +0530

    set matrix zeroes approach-2

--- Data-Structures/arrays/set-matrix-zeroes/index.ts
+++ Data-Structures/arrays/set-matrix-zeroes/index.ts
@@ -1,8 +1,4 @@
 // https://leetcode.com/problems/set-matrix-zeroes/description/
-
-/**
- Do not return anything, modify matrix in-place instead.
- */
 function setZeroes(matrix: number[][]): void {
   const cols = [];
   const rows = [];
@@ -37,8 +33,52 @@ setZeroes(matrix);
 
 console.log(matrix);
 
-
-// approach 2 
+// approach 2
 // without using extra space i.e. eliminate rows[] and cols []
 
+function setMatrixZeros(matrix: number[][]): void {
+  let zeroRowCol = 1;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == 0) {
+        matrix[i][0] = 0;
+        if (j != 0) {
+          matrix[0][j] = 0;
+        } else {
+          zeroRowCol = 0;
+        }
+      }
+    }
+  }
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[i].length; j++) {
+      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
 
+  // mark first row zero
+  if (matrix[0][0] == 0) {
+    for (let i = 1; i < matrix[0].length; i++) {
+      matrix[0][i] = 0;
+    }
+  }
+
+  // mark first col zero
+  if (zeroRowCol == 0) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+}
+
+const matrix2 = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+];
+
+setMatrixZeros(matrix2);
+
+console.log(matrix2);
